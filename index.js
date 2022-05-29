42c2e6897b5430bd9551aff64c7eadadda1d364c
hussain <abdulbaqhussain2018@gmail.com>
2022-05-29 05:58:37 -0700

    my first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const bar = document.querySelector("#bar-menu");
+const listItem = document.querySelector("#hide");
+const list = document.queryCommandValue(".list")
+
+bar.addEventListener("click", (e) => {
+ e.preventDefault();
+ listItem.classList.toggle("active");
+})
