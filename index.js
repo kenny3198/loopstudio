4f5dc519c6b2d8a3add1e01ca851adb66e644914
hussain <abdulbaqhussain2018@gmail.com>
2022-06-03 00:16:59 -0700

    changes commited

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const bar = document.querySelector("#bar-menu");
 const listItem = document.querySelector("#hide");
-const list = document.queryCommandValue(".list")
+const list = document.queryCommandValue(".list");
 
 bar.addEventListener("click", (e) => {
  e.preventDefault();
